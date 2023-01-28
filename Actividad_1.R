6ce75e87d623a36bb2dd8915eeb46a2da9d86fd2
DiegoHoliwis <dimunoz1@uc.cl>
2023-01-28 14:13:30 -0300

    finalizado

--- Actividad_1.R
+++ Actividad_1.R
@@ -82,8 +82,67 @@ url <- pagina %>%
   html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
   html_attr('href')
 
-# Consolidado en una función
+# Consolidado en una función ----
+
+pagina <- read_html("https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-2#origin=qcat&filter_applied=promotion_type&filter_position=3")
+
+# Extraer Número de páginas
+N_paginas <- pagina %>% 
+  html_element(xpath = '//ul[@class = "andes-pagination"]') %>% 
+  html_children() %>% 
+  html_text2() %>% 
+  as.numeric() %>% 
+  max(na.rm = TRUE) %>% 
+  suppressWarnings()
+
+df <- 1:N_paginas %>% 
+  map_dfr(.f = function(k){
+    pagina <- read_html(sprintf('https://www.mercadolibre.cl/ofertas?promotion_type=deal_of_the_day&container_id=MLC779365-2&page=%s',k))
+    
+    nombre <- pagina %>% 
+      html_elements(xpath = '//p[@class = "promotion-item__title"]') %>% 
+      html_text2()
+    
+    precio_anterior <- 1:length(nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = sprintf('//ol[@class = "items_container"]/li[%s]//div[@class = "andes-money-amount-combo promotion-item__price has-discount"]/s/span[3]',x)) %>% 
+          html_text2() %>% 
+          ifelse(is.na(.),'sin precio',.)    
+      })
+    
+    precio_nuevo <- 1:length(nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = sprintf('//ol[@class = "items_container"]/li[%s]//div[@class = "andes-money-amount-combo promotion-item__price has-discount"]/div/span[1]/span[3]',x)) %>% 
+          html_text2() %>% 
+          ifelse(is.na(.),'sin precio nuevo',.)    
+      })
+    
+    envio_gratis <- 1:length(nombre) %>% 
+      map_chr(.f = function(x){
+        pagina %>% 
+          html_element(xpath = sprintf('//ol[@class = "items_container"]/li[%s]//span[@class = "promotion-item__next-day-text"]',x)) %>% 
+          html_text2() %>% 
+          ifelse(is.na(.),'Sin envio gratis',.)    
+      })
+    
+    url <- pagina %>% 
+      html_elements(xpath = '//a[@class = "promotion-item__link-container"]') %>% 
+      html_attr('href')
+    
+    tibble(
+      nombre = nombre,
+      precio_anterior = precio_anterior,
+      precio_nuevo = precio_nuevo,
+      envio_gratis = envio_gratis,
+      url = url
+    )
+    
+  })
+
+df %>% View()
 
 
-
-
+# install.packages('openxlsx') # instalar libreria
+openxlsx::write.xlsx(df, file = 'ofertas mercado libre.xlsx')
---
+++ Actividad_2.R
@@ -0,0 +1,159 @@
+# Actividad número 2 - Selenium
+library(tidyverse)
+library(rvest)
+library(RSelenium)
+
+# library(tidyverse)
+# library(RSelenium)
+# library(netstat)
+# library(Rcpp)
+# library(wdman)
+
+# Configuración inicial (siempre la misma)
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE) # Se finalizan los procesos Java
+
+driver <- RSelenium::rsDriver(browser = "firefox",
+                              chromever = NULL
+)
+remote_driver <- driver[["client"]]
+
+# Iniciamos el web scraping :D
+
+# Conectamos a la página web
+remote_driver$navigate("https://www.santander.cl/cotizador-web/")
+
+MARCA    = 'JEEP'
+MODELO   = 'RENEGADE'
+RUT      = '24312997-4'
+NOMBRE   = 'Diego Muñoz'
+FechaN   = '10/01/1997'
+SEXO     = 'Masculino'
+EMAIL    = 'correo_generico@gmail.com'
+TELEFONO = runif(1,900000000,999999999) %>% round(0) %>% as.character()
+
+# Pagina 1 -----
+# Seleccionar Marca
+
+remote_driver$findElement(using = 'xpath',
+                          value = '//select[@id = "marcas"]/option[@value = "78"]')$clickElement()
+
+
+MARCAS <- remote_driver$findElement(using = 'xpath',
+                                    value = '//select[@id = "marcas"]')$selectTag()
+
+ID_MARCA <- tibble(Marca = MARCAS$text,
+                   ID_MARCA = MARCAS$value) %>% 
+  filter(Marca == MARCA) %>% 
+  slice(1) %>% 
+  pull(ID_MARCA)
+
+remote_driver$findElement(using = 'xpath',
+                          value = paste('//select[@id = "marcas"]/option[@value = ',ID_MARCA,']'))$clickElement()
+
+# Seleccionamos modelo
+MODELOS <- remote_driver$findElement(using = 'xpath',
+                                     value = '//select[@id = "modelos"]')$selectTag()
+
+ID_MODELO <- tibble(Modelo = MODELOS$text,
+                    ID_Modelo = MODELOS$value) %>% 
+  filter(Modelo == MODELO) %>% 
+  slice(1) %>% 
+  pull(ID_Modelo)
+
+remote_driver$findElement(using = 'xpath',
+                          value = paste('//select[@id = "modelos"]/option[@value = ',ID_MODELO,']'))$clickElement()
+
+# Seleccionar Año
+# remote_driver$findElement(using = 'xpath',
+#                           value = '//input[@id = "ano"]')$sendKeysToElement(list('2022'))
+
+remote_driver$findElement(using = 'id',
+                          value = 'ano')$sendKeysToElement(list('2022'))
+
+# Uso
+remote_driver$findElement(using = 'xpath',
+                          value = '//label[@for = "uso:0"]')$clickElement()
+
+# Dueño
+remote_driver$findElement(using = 'xpath',
+                          value = '//label[@for = "duenio:0"]')$clickElement()
+# Siguiente página
+remote_driver$findElement(using = 'id',
+                          value = 'siguiente1')$clickElement()
+
+# Página 2 -----
+
+## Rut  ----
+
+remote_driver$findElement(using = 'id',
+                          value = 'rutCotizanteDueno')$clickElement()
+
+for(i in RUT %>% str_split("") %>% unlist()){
+  remote_driver$findElement(using = 'id',
+                            value = 'rutCotizanteDueno')$sendKeysToElement(list(i))
+  Sys.sleep(runif(1,1,1.8))
+}
+
+# Nombre
+for (i in NOMBRE %>% str_split("") %>% unlist()) {
+  remote_driver$findElement(using = 'id',
+                            value = 'nombre')$sendKeysToElement(list(i))
+  Sys.sleep(runif(1,0.1,0.3))
+}
+
+# Fecha de nacimiento
+for (i in FechaN %>% str_split("") %>% unlist()) {
+  remote_driver$findElement(using = 'id',
+                            value = 'fechaNacimiento_input')$sendKeysToElement(list(i))
+  Sys.sleep(runif(1,0.1,0.3))
+}
+
+# Sexo
+if(SEXO == "Masculino"){
+  remote_driver$findElement(using = 'xpath', value = '//label[@for="sexo:0"]')$clickElement()
+}else{
+  remote_driver$findElement(using = 'xpath', value = '//label[@for="sexo:1"]')$clickElement()
+}
+
+# Email
+for (i in EMAIL %>% str_split("") %>% unlist()) {
+  remote_driver$findElement(using = 'id',
+                            value = 'email')$sendKeysToElement(list(i))
+  Sys.sleep(runif(1,0.1,0.3))
+}
+
+# Telefono
+for (i in TELEFONO %>% str_split("") %>% unlist()) {
+  remote_driver$findElement(using = 'id',
+                            value = 'telefono')$sendKeysToElement(list(i))
+  Sys.sleep(runif(1,0.1,0.3))
+}
+
+remote_driver$findElement(using = 'id',
+                          value = 'siguiente2_1')$clickElement()
+
+# Página 3 ----
+tabla <- remote_driver$findElement(using = "id",
+                                   value = "matriz")$getElementAttribute('innerHTML')[[1]] %>% 
+  read_html()
+
+precios <- tabla %>% 
+  html_table() %>% 
+  map_dfc(.f = function(x){
+    x %>% 
+      select(str_subset(names(.),'Deducible'))
+  }) %>% 
+  filter(`Deducible 0 UF` != 'Sin Producto')
+
+
+compañias <- remote_driver$findElement(using = "xpath",
+                                       value = '//div[@class = "column-table40"]')$getElementAttribute('innerHTML')[[1]] %>% 
+  read_html() %>% 
+  html_elements(xpath = '//tr[@class = "filaMatriz"]/td[1]/img') %>% 
+  html_attr('alt')
+
+compañias
+
+
+precios %>% 
+  add_column(compañias)
